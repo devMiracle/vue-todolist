5ca9844c05ff822b35f115fa9cba6b51a9166cb1
Miracle <51058450+devMiracle@users.noreply.github.com>
2020-12-13 14:53:53 +0200

    done

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, tempora.</p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
+
+
